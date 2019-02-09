d7f1b8b1b83093b942d4927a4415ba3185e745c7
Ananta Pratama <anantapratama@Anantas-MacBook-Pro.local>
2019-02-09 11:28:01 +0700

    Setup react router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import { Route, Link, BrowserRouter as Router } from "react-router-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Catalog from "./pages/Catalog"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routing = (
+  <Router>
+    <div>
+      <Route path="/" component={Catalog}/>
+    </div>
+  </Router>
+)
+
+export default ReactDOM.render(routing, document.getElementById("root"))
